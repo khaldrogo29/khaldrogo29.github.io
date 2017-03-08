5b004a3b563174545041bfc18329b77e4071db26
khaldrogo29 <gangal.chinmay@gmail.com>
2017-03-08 19:17:19 +0530

    Update khaldrogo_main.js

--- js/khaldrogo_main.js
+++ js/khaldrogo_main.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
  	$(this).focus();
   $(this).css("border-color","yellow");
  });
+ 
+  $('.txt_in').mouseleave(function(){
+ 	$(this).blur();
+  $(this).css("border-color","grey");
+ });
+
 
  $('#submit_btn').click(function(){
  	         alert("Your response is sent. Thankyou for you suggestions");
