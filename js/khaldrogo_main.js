bb2611c9ed89586451ad4ee00263cd2bda9d9c01
khaldrogo29 <gangal.chinmay@gmail.com>
2017-03-08 19:04:03 +0530

    Add files via upload

---
+++ js/khaldrogo_main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+ $('.txt_in').mouseenter(function(){
+ 	$(this).focus();
+ });
+
+ $('#submit_btn').click(function(){
+ 	         alert("Your response is sent. Thankyou for you suggestions");
+
+ })
+
+});
